c192e5e88ff6f0c18570ca9cf396581f96784d2a
Wx11y <willmattos.c@gmail.com>
2023-02-02 13:13:52 +0100

    inicios buenos

---
+++ src/Controller/Metodos.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Controller;
+// src/Controller/PedidosLogin.php
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Address;
+
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+
+class Metodos extends AbstractController{
+    /**
+     * @Route("/bandeja", name="bandeja")
+     */
+    public function mostrarCategorias() {
+        return new Response("entro");
+    }
+}
