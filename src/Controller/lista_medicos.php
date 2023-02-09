9b1e3116236a31f5876153772dd9dcbcfea928d8
Wx11y <willmattos.c@gmail.com>
2023-02-09 13:45:25 +0100

    tt

---
+++ src/Controller/lista_medicos.php
@@ -0,0 +1,25 @@
+<?php
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Address;
+
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use App\Entity\Consulta;
+use App\Entity\Especialidades;
+use App\Entity\Medico;
+use App\Entity\Mensaje;
+use App\Entity\Usuario;
+use App\Entity\Valoran;
+// Con findBy/findOneBy: jugadores con exactamente 12 años
+$entityManager = $this->getDoctrine()->getManager();
+$especialidad = $entityManager->find(Especialidad::class,12);
+$medicos = $entityManager->getRepository('Medico')->findBy(array('especialidad' => $especialidad));
+var_dump($medicos);
